69b45bbfa78f305f707a3e3d5dccb156e7a8f70a
chenxiaoming <547480202@qq.com>
2017-08-11 22:11:39 +0800

    添加修改
    添加了手机端的考试页面

--- resources/views/admin/exam/paper.blade.php
+++ resources/views/admin/exam/paper.blade.php
@@ -1,17 +1,17 @@
 @extends('layout.admin')
 
 @section('content')
-    <div ng-controller="exam-detail.controller as ctrl">
+<!--     <div ng-controller="exam-detail.controller as ctrl">
         <div class="list-group" ng-repeat="x in ctrl.paper">
-            <div class="list-group-item active">
+            <div class="list-group-item default">
             	<button type="button" class="close danger" aria-label="Close" ><span aria-hidden="true">&times;</span></button>
-	           	<h4 class="list-group-item-heading" >{$x.content$}</h4>    
+	           	<h4 class="list-group-item-heading" >{$ $index+1 + "." + x.content$}</h4>   
                 <p class="list-group-item-text">
                     <div class="radio">
                         <form>
-        	              	<ul  ng-repeat="y in x.options track by $index">                          
+        	              	<ul  ng-repeat="y in x.options track by $index"  style="display: inline">                          
                                 <label for="">
-                                    <input type="radio" name="checkOptions"><li>{$y.content$}</li>
+                                    <input type="radio" name="checkOptions"><li>{$ $index+1 + "." + y.content$}</li>
                                 </label>
         	                </ul>
                         </form>
@@ -19,6 +19,29 @@
                 </p>   
             </div>
         </div>
-    </div>
+    </div> -->
     <input id="paperId" value='{{ $id }}' hidden>
+
+    <!-- 手机端 -->
+    <br>
+    <br>
+    <div ng-controller="exam-detail.controller as ctrl" style="margin-right:150px; ">
+        <div class="list-group" ng-repeat="x in ctrl.paper">
+            <div class="list-group-item default">
+                <h4 class="list-group-item-heading" >{$ $index+1 + "." + x.content$}</h4>   
+                <p class="list-group-item-text">
+                    <div class="radio">
+                        <form>
+                            <ul  ng-repeat="y in x.options track by $index"  style="display: inline">                          
+                                <label for="">
+                                    <input type="radio" name="checkOptions"><li>{$ $index+1 + "." + y.content$}this is a test! this is a test</li>
+                                </label>
+                            </ul>
+                        </form>
+                    </div>
+                </p>   
+            </div>
+        </div>
+        <button class="btn btn-default form-inline">提交</button>
+    </div>
 @endsection
